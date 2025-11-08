3775ae1ca8fe8a9de2f8837f34eaa94fa83c21a0
Ahmad Shalabi <ashalabi@atypon.com>
2025-11-08 18:26:07 +0300

    chore(frontend): configure routing and tailwind styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import { HomePage } from './pages/HomePage'
+import { AboutPage } from './pages/AboutPage'
+import { RecipesPage } from './pages/RecipesPage'
 
 export const App = () => {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/recipes" element={<RecipesPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   )
